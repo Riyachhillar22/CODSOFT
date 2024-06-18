42685dfd4592c9054a073b43a58bbbff410174a7
Riya <151901865+Riyachhillar22@users.noreply.github.com>
2024-06-18 22:29:41 +0530

    Add files via upload

---
+++ landing page/script.js
@@ -0,0 +1,99 @@
+const toggleMiniMenu = document.querySelector('i.bx-menu')
+const miniNav = document.querySelector('.nav-list')
+
+let navbar = document.querySelector('.navbar');
+document.querySelector('#menu-bar').onclick=() =>{
+    navbar.classList.toggle('active');
+}
+
+
+
+let search = document.querySelector('.search');
+document.querySelector('#search').onclick=() =>{
+    search.classList.toggle('active');
+}
+
+
+var swiper = new Swiper(".product-row", {
+    spaceBetween: 30,
+    loop:true,
+    centeredSlides:true,
+    autoplay:{
+        delay:9500,
+        disableOnInteraction:false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+var swiper = new Swiper(".blogs-row", {
+    spaceBetween: 30,
+    loop:true,
+    centeredSlides:true,
+    autoplay:{
+        delay:9500,
+        disableOnInteraction:false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation:{
+        nextE1 :".swiper-button-next",
+        prevE1 :".swiper-button-prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 1,
+      },
+      1024: {
+        slidesPerView: 1,
+      },
+    },
+  });
+
+  var swiper = new Swiper(".review-row", {
+    spaceBetween: 30,
+    loop:true,
+    centeredSlides:true,
+    autoplay:{
+        delay:9500,
+        disableOnInteraction:false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+toggleMiniMenu.addEventListener('click', () => {
+    miniNav.classList.toggle('open')
+
+    const isOpen = miniNav.classList.contains('open')
+
+})
